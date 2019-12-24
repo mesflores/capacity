03460a46b465956140f6aa64b28f88e513e99de8
Marcel <marcel@parsons.home.brickporch.com>
2019-12-24 10:13:37 -0800

    add some args to the test run script

--- test_run.sh
+++ test_run.sh
@@ -1,20 +1,48 @@
 #!/usr/local/bin/bash
 
+usage() {
+    echo -e "\nUsage:\n test_run.sh <routes> <mat> [days] [processes] [reps]\n" 
+    exit 1
+}
+
 if [  $# -le 1 ] 
 then 
-    echo -e "\nUsage:\n test_run.sh <routes> <mat>\n" 
-    exit 1
+	usage
 fi 
 
 # Make a dir for nodes to dump data to
 mkdir -p out_dat
 
 MAXDIFF=4
-MAXTS=400000
 
+
+# Parse all the args
 ROUTES=$1
 MAT=$2
 
+if [ "$#" -eq 2 ]; then
+	DAYS=1
+	PROCS=4
+	REPS=1
+elif [ "$#" -eq 3 ]; then
+	DAYS=$3
+	PROCS=4
+	REPS=1
+elif [ "$#" -eq 4 ]; then
+	DAYS=$3
+	PROCS=$4
+	REPS=1
+elif [ "$#" -eq 5 ]; then
+	DAYS=$3
+	PROCS=$4
+	REPS=$5
+else
+	usage
+fi
+
+MAXTS=`expr 86400 \* $DAYS`
+
+echo "Executing for $MAXTS ($DAYS days) on $PROCS proccess with $REPS optimistic reps..."
 
 # Clean up leftovers
 rm -f base.out seq.raw seq.out con.raw con.out opt.raw opt.out
@@ -32,7 +60,7 @@ mpirun -np 1 models/capacity/src/capacity --synch=1 --mat=$MAT --routes=$ROUTES 
 cat seq.raw | grep \\\[ | grep -v "Total KPs" | sort -n -t[ -k2 > seq.out
 
 echo "Running Conservative..."
-mpirun -np 4 models/capacity/src/capacity --synch=2 --mat=$MAT --routes=$ROUTES --end=$MAXTS  > con.raw
+mpirun -np $PROCS models/capacity/src/capacity --synch=2 --mat=$MAT --routes=$ROUTES --end=$MAXTS  > con.raw
 cat con.raw | grep \\\[ | grep -v "Total KPs" | sort -n -t[ -k2 > con.out
 
 # Compare them
@@ -46,9 +74,9 @@ fi
 
 echo "Running Optimistic..."
 # Run it optimistic, lots of times
-for i in {1..1}
+for i in {1..$REPS}
     do
-        mpirun -np 4 models/capacity/src/capacity --synch=3 --mat=$MAT --routes=$ROUTES --end=$MAXTS --extramem=1024 > opt.raw
+        mpirun -np $PROCS models/capacity/src/capacity --synch=3 --mat=$MAT --routes=$ROUTES --end=$MAXTS --extramem=1024 > opt.raw
         # break out if it fails
         if [ "$?" -ne 0 ]
         then
